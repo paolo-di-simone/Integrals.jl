45b368fe797b00f94ae599d1a189585719a2eba9
paolo-di-simone <72343829+paolo-di-simone@users.noreply.github.com>
2022-04-23 19:39:32 +0200

    secondo commit

--- progettoSenzaDipendenze/integrals.jl
+++ progettoSenzaDipendenze/integrals.jl
@@ -16,7 +16,22 @@ function M(alpha::Int, beta::Int)::Float64
     return a/(alpha + 1)
 end
 
+# Quanto diminuisce la velocità di calcolo se invece di un'approssimazione Float64 si usa una Float128?
+# Documentare molto i test
+# Utilizzare librerie ad-hoc per l'hardware sottostante
+
+
+# P = modello poliedrale, nel quale si va ad integrare (descritto mediante la sua superficie fatta di triangoli)
+# alpha, beta e gamma esponenti di un monomio in x, y e z (si sta integrando un monomio del tipo x^alpha * y ^ beta * z ^ gamma su un insieme di triangoli)
+
+# Funzione principale di integrazione sul singolo triangolo
+# Un triangolo una matrice 3x3 di coordinate
+# Da quello che ho capito viene fatto l'integrale di superficie del triangolo, quindi viene calcolata l'area del triangolo (e non un volume)
+# L'algoritmo porta ad un errore di approssimazione alla terza cifra decimale (trascurabile?)
+
+# In generale l'algoritmo non l'ho capito
 function TT(tau::Array{Float64,2}, alpha::Int, beta::Int, gamma::Int, signedInt::Bool=false)
+	# tau = triangolo
 	vo,va,vb = tau[:,1],tau[:,2],tau[:,3]
 	a = va - vo
 	b = vb - vo
@@ -47,6 +62,10 @@ function TT(tau::Array{Float64,2}, alpha::Int, beta::Int, gamma::Int, signedInt:
 		return s1 * norm(c)
 	end	
 end
+
+# Da quello che ho capito abbbiamo V e FV che rappresentano (vedi PDF):
+#		- FV due celle 
+#		- V uno celle
 
 function II(P::LAR, alpha::Int, beta::Int, gamma::Int, signedInt=false)::Float64
     w = 0
@@ -85,6 +104,9 @@ function III(P::LAR, alpha::Int, beta::Int, gamma::Int)::Float64
     end
     return w/(alpha + 1)
 end
+
+
+
 
 function surface(P::LAR, signedInt::Bool=false)::Float64
     return II(P, 0, 0, 0, signedInt)

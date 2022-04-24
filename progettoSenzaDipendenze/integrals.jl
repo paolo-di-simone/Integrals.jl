0bacf15bc2c110f13dc709d97d478b392f464648
Paolo Di Simone <72343829+paolo-di-simone@users.noreply.github.com>
2022-04-24 15:56:45 +0200

    Update integrals.jl

--- progettoSenzaDipendenze/integrals.jl
+++ progettoSenzaDipendenze/integrals.jl
@@ -7,7 +7,6 @@ const Cells = Array{Array{Int,1},1}
 
 const LAR = Union{ Tuple{Points, Cells},Tuple{Points, Cells, Cells} }
 
-
 function M(alpha::Int, beta::Int)::Float64
     a = 0
     for l=0:(alpha + 1)
@@ -16,22 +15,7 @@ function M(alpha::Int, beta::Int)::Float64
     return a/(alpha + 1)
 end
 
-# Quanto diminuisce la velocità di calcolo se invece di un'approssimazione Float64 si usa una Float128?
-# Documentare molto i test
-# Utilizzare librerie ad-hoc per l'hardware sottostante
-
-
-# P = modello poliedrale, nel quale si va ad integrare (descritto mediante la sua superficie fatta di triangoli)
-# alpha, beta e gamma esponenti di un monomio in x, y e z (si sta integrando un monomio del tipo x^alpha * y ^ beta * z ^ gamma su un insieme di triangoli)
-
-# Funzione principale di integrazione sul singolo triangolo
-# Un triangolo una matrice 3x3 di coordinate
-# Da quello che ho capito viene fatto l'integrale di superficie del triangolo, quindi viene calcolata l'area del triangolo (e non un volume)
-# L'algoritmo porta ad un errore di approssimazione alla terza cifra decimale (trascurabile?)
-
-# In generale l'algoritmo non l'ho capito
 function TT(tau::Array{Float64,2}, alpha::Int, beta::Int, gamma::Int, signedInt::Bool=false)
-	# tau = triangolo
 	vo,va,vb = tau[:,1],tau[:,2],tau[:,3]
 	a = va - vo
 	b = vb - vo
@@ -62,10 +46,6 @@ function TT(tau::Array{Float64,2}, alpha::Int, beta::Int, gamma::Int, signedInt:
 		return s1 * norm(c)
 	end	
 end
-
-# Da quello che ho capito abbbiamo V e FV che rappresentano (vedi PDF):
-#		- FV due celle 
-#		- V uno celle
 
 function II(P::LAR, alpha::Int, beta::Int, gamma::Int, signedInt=false)::Float64
     w = 0
@@ -105,13 +85,9 @@ function III(P::LAR, alpha::Int, beta::Int, gamma::Int)::Float64
     return w/(alpha + 1)
 end
 
-
-
-
 function surface(P::LAR, signedInt::Bool=false)::Float64
     return II(P, 0, 0, 0, signedInt)
 end
-
 
 function volume(P::LAR)::Float64
     return III(P, 0, 0, 0)
@@ -141,11 +117,9 @@ function inertiaProduct(P::LAR)::Array{Float64,1}
     return out
 end
 
-
 function centroid(P::LAR)::Array{Float64,1}
 	return firstMoment(P)./volume(P)
 end
-
 
 function inertiaMoment(P::LAR)::Array{Float64,1}
     out = zeros(3)
@@ -156,12 +130,10 @@ function inertiaMoment(P::LAR)::Array{Float64,1}
     return out
 end
 
-
 function chainAreas(V::Array{Float64,2},EV::Array{Int64,2},chains::Array{Int64,2})
 	FE = [chains[:,f] for f=1:size(chains,2)]
 	return chainAreas(V,EV,FE)
 end
-
 
 function chainAreas(V::Array{Float64,2}, EV::Array{Int64,2}, chains::Array{Array{Int64,1},1})
 	if size(V,1) == 2
@@ -185,4 +157,3 @@ function chainAreas(V::Array{Float64,2}, EV::Array{Int64,2}, chains::Array{Array
 	end
 	return out
 end
-
